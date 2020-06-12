4418900a1bddfdd10252afd371aae0e1513c9364
paulflp <49452698+paulflp@users.noreply.github.com>
2020-06-12 17:26:20 +0300

    Source code added

---
+++ hamming.asm
@@ -0,0 +1,248 @@
+.386
+.model flat, stdcall
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;includem biblioteci, si declaram ce functii vrem sa importam
+includelib msvcrt.lib
+extern exit: proc
+extern fscanf: proc
+extern printf: proc
+extern fprintf: proc
+extern scanf: proc
+extern fopen: proc
+extern fclose: proc
+extern fseek: proc
+include mylib.asm
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;declaram simbolul start ca public - de acolo incepe executia
+public start
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;sectiunile programului, date, respectiv cod
+.data
+mode_read DB "r",0
+mode_write DB "w",0
+format DB "%s",0
+file_name DB "fisier.in",0
+fileout_name DB "rez.out",0
+decimal DB "%d",0
+sir DB 32 DUP(30h),0
+sir_hamming DB 38 DUP(48),0
+adresa_fisier_citire DD 0
+adresa_fisier_scriere DD 0
+message_correct DB "Correct!",0
+message_incorrect DB "Error on bit %d",0
+message_wrong DB "Incorect!",0
+message_corrected_array DB "Correct: %s",0
+options DB "Choose one option:",10,"1 - Generate Hamming Code",10,"2 - Check Hamming Code",10,0
+val_option DD 0
+wrongbit DD 0
+newline DD 10
+nr_adrese DD 1024
+var_seek_cur dd 1
+var2 dd 2
+;aici declaram date
+
+.code
+reinit_ham proc
+push EBP
+mov EBP,ESP
+
+mov ESI,[EBP+8]
+mov al,30h
+
+mov [ESI],al
+
+add ESI,1
+mov [ESI],al
+
+add ESI,2
+mov [ESI],al
+
+add ESI,4
+mov [ESI],al
+
+add ESI,8
+mov [ESI],al
+
+add ESI,16
+mov [ESI],al
+
+mov ESP,EBP
+pop EBP
+ret 4
+reinit_ham endp
+start:
+	;aici se scrie codul
+	;deschidere_fisier_citire:
+	push offset options
+	push offset format
+	call printf
+	add ESP,8
+	
+	push offset val_option
+	push offset decimal
+	call scanf
+	add ESP,8
+	
+	mov ebx,val_option
+	cmp ebx,2
+	je checkoption
+generateoption:	
+	push offset mode_read
+	push offset file_name
+	call fopen
+	add ESP,8
+	mov adresa_fisier_citire,EAX
+	
+	push offset mode_write
+	push offset fileout_name
+	call fopen
+	add ESP,8
+	mov adresa_fisier_scriere,EAX
+	
+	mov ECX,nr_adrese
+	
+citire_sir:
+	push ECX
+	
+	push offset sir
+	push offset format
+	push adresa_fisier_citire
+	call fscanf
+	add ESP,12
+	
+	push offset sir_hamming
+	call reinit_ham
+	
+	push offset sir_hamming
+	push offset sir
+	call generate_hamming
+	
+	push offset sir_hamming
+	push offset format
+	call printf
+	add ESP,8
+	
+	push offset newline
+	push offset format
+	call printf
+	add ESP,8
+	
+	push offset sir_hamming
+	push offset format
+	push adresa_fisier_scriere
+	call fprintf
+	add ESP,12
+	
+	push offset newline
+	push offset format
+	push adresa_fisier_scriere
+	call fprintf
+	add ESP,12
+	
+	pop ECX
+	sub ECX,1
+	cmp ECX,0
+	jnz citire_sir
+	jmp finalizare
+	
+checkoption:
+	push offset mode_read
+	push offset fileout_name
+	call fopen
+	add ESP,8
+	mov adresa_fisier_scriere,eax
+	
+	mov ECX,nr_adrese
+citire_sir_hamming:
+	push ECX
+	
+	push offset sir_hamming
+	push offset format
+	push adresa_fisier_scriere
+	call fscanf	
+	add ESP,12
+	
+	;push var_seek_cur
+	;push var2
+	;push adresa_fisier_scriere
+	;call fseek
+	;add ESP,12
+	;add var2,40
+	
+	mov EDX,0
+	push offset sir_hamming
+	call check_hamming
+	
+	mov wrongbit,edx
+	
+	cmp wrongbit,0
+	je no_error
+show_error:
+	push offset message_wrong
+	push offset format
+	call printf
+	add ESP,8
+	
+	push offset newline
+	push offset format
+	call printf
+	add ESP,8
+	
+	push wrongbit
+	push offset message_incorrect
+	call printf
+	add ESP,8
+	
+	push offset newline
+	push offset format
+	call printf
+	add ESP,8
+	
+	lea ESI,sir_hamming
+	add ESI,wrongbit
+	sub ESI,1
+	
+	mov bl,30h
+	mov cl,31h
+	
+	mov AL,[ESI]
+	cmp AL,30h
+	je process1
+	
+	mov [ESI],bl
+	jmp skip1
+process1:
+	mov [ESI],cl
+skip1:
+	push offset sir_hamming
+	push offset message_corrected_array
+	call printf
+	add ESP,8
+	push offset newline
+	push offset format
+	call printf
+	add ESP,8
+	jmp dont_print_ok
+no_error:
+	push offset message_correct
+	push offset format
+	call printf
+	add ESP,8
+	push offset newline
+	push offset format
+	call printf
+	add ESP,8
+	
+dont_print_ok:
+	pop ECX
+	sub ECX,1
+	cmp ECX,0
+	jnz citire_sir_hamming
+finalizare:
+	;terminarea programului
+	push 0
+	call exit
+end start
---
+++ mylib.asm
@@ -0,0 +1,365 @@
+.code
+generate_hamming proc
+push EBP
+mov EBP,ESP
+
+mov ESI,[EBP+8] ;luam primul argument care este adresa sirului sursa
+mov EDI,[EBP+12] ; luam al doilea argument care este adresa sirului in care dorim sa scriem datele
+;scriere_date_in_sir_hamming(se copiaza din sirul de date bitii pe pozitiile din codul hamming)
+	add EDI,2
+	mov ECX,1
+	rep movsb ;se scrie bitul 3
+	
+	add EDI,1
+	mov ECX,3
+	rep movsb; se scriu bitii 5-7
+	
+	add EDI,1
+	mov ECX,7
+	rep movsb ; se scriu bitii 9-15
+	
+	add EDI,1
+	mov ECX,15
+	rep movsb ;se scriu bitii 17-31
+	
+	add EDI,1
+	mov ECX,6
+	rep movsb ;se scriu bitii 33-38
+;calculare P1 - se calculeaza bitul de pe pozitia 1 (xor intre 1,3,5,7,...) - se incepe de la poz1
+	mov ESI,[EBP+12]
+	mov ecx,19
+	xor al,al
+	calcul_p1:
+		mov bl,[ESI]
+		sub bl,30h
+		xor al,bl
+		add ESI,2
+		loop calcul_p1
+		
+		
+	mov ESI,[EBP+12]
+	add AL,30h
+	mov [ESI],AL
+	
+;calculare P2 - se calculeaza bitul de pe pozitia 2(xor intr 2,3,6,7,10,11,...) se iau cate 2, apoi se sare peste 2 - se incepe de la poz2
+	mov ESI,[EBP+12]
+	add ESI,1
+	mov ECX,9
+	xor al,al
+	calcul_p2:
+		mov bl,[ESI]
+		sub bl,30h
+		xor al,bl
+		add ESI,1
+		mov bl,[ESI]
+		sub bl,30h
+		xor al,bl
+		add ESI,3
+		loop calcul_p2
+	mov bl,[esi]
+	sub bl,30h
+	xor al,bl
+	mov ESI,[EBP+12]
+	add ESI,1
+	add al,30h
+	mov [ESI],al
+	
+;calculare P4 - se calculeaza bitul de pe pozitita 4(xor intre 4,5,6,7,12,13,14,15,...) se iau 4,apoi se sare peste 4 - se incepe de la poz4
+mov ESI,[EBP+12]
+add ESI,3
+mov ECX,4
+xor al,al
+calcul_p4:
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	
+	add ESI,5
+	loop calcul_p4
+	
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	
+	mov ESI,[EBP+12]
+	add ESI,3
+	add al,30h
+	mov [ESI],al
+
+;calculare P8 - se calculeaza bitul de pe pozitia 8 - xor intre 8 biti, apoi se trece peste alti 8 samd - se incepe de la poz8
+mov ESI,[EBP+12]
+add ESI,7
+mov ECX,8
+xor al,al
+b8to15:
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	loop b8to15
+add ESI,8
+mov ECX,8
+b24to31:
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+loop b24to31
+
+mov ESI,[EBP+12]
+add ESI,7
+add al,30h
+mov [ESI],al
+
+;calculare P16 - xor intre poz 16-31
+mov ESI,[EBP+12]
+add ESI,15
+mov ECX,16
+xor al,al
+b16to31:
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	loop b16to31
+
+mov ESI,[EBP+12]
+add ESI,15
+add al,30h
+mov [ESI],al
+
+;calculare P32 - xor intre  poz 32-38
+mov ESI,[EBP+12]
+add ESI,31
+mov ECX,7
+xor al,al
+b32to38:
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	loop b32to38
+
+mov ESI,[EBP+12]
+add ESI,31
+add al,30h
+mov [ESI],al
+
+mov ESP,EBP
+pop EBP
+ret 8
+generate_hamming endp
+
+check_hamming proc
+
+push EBP
+mov EBP,ESP
+
+xor EDX,EDX
+mov ESI,[EBP+8] ;luam de pe stiva adresa sirului hamming
+
+;calcul p1
+	mov ecx,19
+	xor al,al
+	calcul_p1:
+		mov bl,[ESI]
+		sub bl,30h
+		xor al,bl
+		add ESI,2
+		loop calcul_p1
+
+		cmp al,00h
+		je continue1
+adauga_1:
+	add dl,01h
+continue1:
+	add dl,0
+
+;calcul p2
+mov ESI,[EBP+8]
+add ESI,1
+	mov ECX,9
+	xor al,al
+	calcul_p2:
+		mov bl,[ESI]
+		sub bl,30h
+		xor al,bl
+		add ESI,1
+		mov bl,[ESI]
+		sub bl,30h
+		xor al,bl
+		add ESI,3
+		loop calcul_p2
+	mov bl,[esi]
+	sub bl,30h
+	xor al,bl
+
+	cmp al,00h
+	je continue2
+adauga_2:
+	add dl,02h
+continue2:
+	add dl,0
+
+;calcul p4
+mov ESI,[EBP+8]
+add ESI,3
+mov ECX,4
+xor al,al
+calcul_p4:
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	
+	add ESI,5
+	loop calcul_p4
+	
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+
+	cmp al,00h
+	je continue4
+adauga_4:	
+	add dl,04h
+continue4:
+	add dl,0
+
+;calcul p8
+mov ESI,[EBP+8]
+add ESI,7
+mov ECX,8
+xor al,al
+b8to15:
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	loop b8to15
+add ESI,8
+mov ECX,8
+b24to31:
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+loop b24to31
+	
+	cmp al,00h
+	je continue_8
+adauga_8:
+	add dl,08h
+continue_8:
+	add dl,0
+;calcul p16
+mov ESI,[EBP+8]
+add ESI,15
+mov ECX,16
+xor al,al
+b16to31:
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	loop b16to31
+
+	cmp al,00h
+	je continue_16
+adauga_16:
+	add dl,10h
+continue_16:
+	add dl,0
+
+
+;calcul p32
+mov ESI,[EBP+8]
+add ESI,31
+mov ECX,7
+xor al,al
+b32to38:
+	mov bl,[ESI]
+	sub bl,30h
+	xor al,bl
+	add ESI,1
+	loop b32to38
+	
+	cmp al,00h
+	je continue_32
+adauga_32:
+	add dl,20h
+continue_32:
+	add dl,0
+
+mov ESP,EBP
+pop EBP
+ret 4
+check_hamming endp
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
